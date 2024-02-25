66131099bab584c4cfa7b3814a4de3fbc6c84ec1
Henri GEY BECHALIER <henri.gey-bechalier@epitech.eu>
2024-02-25 23:16:49 +0100

    [+]fix memmove

--- memmove.asm
+++ memmove.asm
@@ -8,20 +8,19 @@ memmove:
     jmp .right_to_left
 
 .left_to_right:
-    cmp rdx, 0
+    cmp rcx, rdx
         je .end
     mov r8b, byte [rsi + rcx]
     mov byte [rdi + rcx], r8b
     inc rcx
-    dec rdx
     jmp .left_to_right
 
 .right_to_left:
     cmp rdx, 0
         je .end
-    mov r8b, byte [rsi + rdx - 1]
-    mov byte [rdi + rdx - 1], r8b
     dec rdx
+    mov r8b, byte [rsi + rdx]
+    mov byte [rdi + rdx], r8b
     jmp .right_to_left
 
 .end:
